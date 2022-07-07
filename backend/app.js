dffe818b513ed109d3946eba8a5c9cc6221f65d8
Diana Kamensky <dianakamensky@gmail.com>
2022-07-07 17:04:59 +0300

    add celebrate to login

--- backend/app.js
+++ backend/app.js
@@ -10,10 +10,32 @@ const { requestLogger, errorLogger } = require("./middleware/logger");
 const { handleError } = require("./utils/errors");
 const { login, createUser } = require("./controllers/users");
 const { errors } = require("celebrate");
+const { Joi, celebrate } = require("celebrate");
 mongoose.connect("mongodb://localhost:27017/aroundb");
 app.use(requestLogger);
-app.post("/signin", login);
-app.post("/signup", createUser);
+app.post(
+  "/signin",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login
+);
+app.post(
+  "/signup",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string(),
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUser
+);
 app.use(auth);
 app.use("/", router);
 require("./routes/cards");
--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -2,13 +2,22 @@ const winston = require("winston");
 const expressWinston = require("express-winston");
 
 const requestLogger = expressWinston.logger({
-  transports: [new winston.transports.File({ filename: "request.log" })],
+  transports: [
+    new winston.transports.File({ filename: "request.log" }),
+    new winston.transports.Console(),
+  ],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
-  transports: [new winston.transports.File({ filename: "error.log" })],
-  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: "error.log" }),
+    new winston.transports.Console(),
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
 });
 
 module.exports = {
