d84c4850386f91d4aec469dd2a25bd168632599b
Diana Kamensky <dianakamensky@gmail.com>
2022-07-07 10:46:58 +0300

    remove old user auth

--- backend/app.js
+++ backend/app.js
@@ -7,14 +7,6 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "629b8ecc3e174874c0ec0334",
-  };
-
-  next();
-});
-
 mongoose.connect("mongodb://localhost:27017/aroundb");
 
 app.post("/signin", login);
