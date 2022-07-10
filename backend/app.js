3a6ad749860cb785ee8ea41abf1138c959f74c73
Diana Kamensky <dianakamensky@gmail.com>
2022-07-10 17:50:51 +0300

    add email validation

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.post(
   "/signin",
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required(),
+      email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
@@ -41,7 +41,7 @@ app.post(
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().uri(),
-      email: Joi.string().required(),
+      email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
